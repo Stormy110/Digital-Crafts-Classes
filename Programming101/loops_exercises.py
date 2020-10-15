8918723b4a1e3188e33b8aa3f3803b9658956dcb
Ian Storms <IanStorms110@yahoo.com>
2020-10-14 21:55:58 -0400

    fixed loops

--- Programming101/loops_exercises.py
+++ Programming101/loops_exercises.py
@@ -1,13 +1,13 @@
 
-i = 1
-while i < 11:
-    print(i) 
-    i += 1
+# i = 1
+# while i < 11:
+#     print(i) 
+#     i += 1
 
-j = 10
-while j > 0:
-    print(j)
-    j -= 1
+# j = 10
+# while j > 0:
+#     print(j)
+#     j -= 1
 
 #user_name = ""
 #password = ""
@@ -26,16 +26,18 @@ while j > 0:
 # now i will add a maximum number of attempts allowed before exiting the loop
 
 
-user_name = "Ian"
-password = "banana"
-name = ""
+
+user_name = ""
 pwd = ""
-count = 0
-while name != user_name and pwd != password and count != 3:
-    name = input("What is your name?\n")
-    pwd = input("What is the password?\n")
-    count += 1
-    if count == 3:
-        print("You have no more chances")
-print("You may proceed")
- #### this is not correct, need to fix
+i = 0
+
+while i <= 2 and (user_name != "Ian" or pwd != "banana"):
+    user_name = input("What is the user name? ")
+    pwd = input("What is the password? ")
+    i += 1
+    if i == 2:
+        print("You suck!")
+        break
+    if user_name == "Ian" and pwd == "banana":
+        print(f"Welcome {user_name}! So glad you could make it!")
+        break
