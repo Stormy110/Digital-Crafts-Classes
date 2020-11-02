af3ecc5d5b12a30e9b50638a90a0f8b8b44444d9
Ian Storms <IanStorms110@yahoo.com>
2020-11-02 12:23:49 -0500

    javascript

---
+++ JavaScript/JavaScript Exercises/variable-exercises.js
@@ -0,0 +1,10 @@
+let num = 1;
+let num2 = 15;
+let num3 = 250;
+// These 3 vqriabloes ahve been assigned values.
+
+const name = "Ian";
+// This assigns the value of "Ian" to the const variable name
+
+let a = "hello", b = "goodbye", c = "yessir"
+console.log(a,b,c)
---
+++ JavaScript/Variables.js
@@ -0,0 +1,26 @@
+var a;//declare before assigning
+a = 10;//assigning a declared variable
+var b = 10; //declaring and assigning
+b = 23; //re-assign variable
+/* var allows you to re-declare a variable inside of a function. This can and will lead to bugs and should no longer be used */
+
+let = a;
+let a = "Something" //ending simi-colon not required...
+a = 1
+/* let does not allow you to redeclare inside of a function. It should be used in place of var*/
+
+const a = 'someintin'
+a = 'something esle'//error cannot re-assign const.
+
+let camelCaseWin = 'Camel Case for the win'
+
+//declare multiple
+let a,b,c
+a = 1;
+b = 2;
+c = 3;
+
+//declaring and assigning multiple
+let e,f=10,g=12
+//e is undefined f is 10 g is 12
+
