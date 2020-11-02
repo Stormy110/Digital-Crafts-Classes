e498b01dfe41276b60131007be02c378f375a2c1
Ian Storms <IanStorms110@yahoo.com>
2020-11-02 16:20:41 -0500

    js practice

---
+++ JavaScript/JavaScript Exercises/loops-exercises.js
@@ -0,0 +1,19 @@
+let i = 1;
+let sum = 0;
+while (i <= 10) {
+    sum += i;
+    i++;
+}
+console.log(sum)
+
+
+for(let j = 25; j <= 40; j++){
+    console.log(j)
+}
+
+
+let number = 75;
+for(let i = 6; i >= 2; i--){
+    number /= i;
+    console.log(number)
+}
