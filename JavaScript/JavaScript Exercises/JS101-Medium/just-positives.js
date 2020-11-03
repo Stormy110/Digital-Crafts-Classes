628c2a9ffa33ac0db4cf8f94de3ddb784303185a
Ian Storms <IanStorms110@yahoo.com>
2020-11-03 18:03:20 -0500

    exercises

---
+++ JavaScript/JavaScript Exercises/JS101-Medium/just-positives.js
@@ -0,0 +1,10 @@
+function positiveNumbers(numbers){
+    newNumbers = [];
+    for(number of numbers){
+        if (number > 0){
+            newNumbers.push(number)
+        } 
+    }
+    console.log(newNumbers)
+}
+positiveNumbers([5, -3, 8, 27, -6, -9, -3, 65, 13])
